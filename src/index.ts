e6707fc6b3f7082205c784ea0102c7a05c2444a8
piman51277 <piman.dev@gmail.com>
2021-12-01 12:51:21 -0500

    add SquaredError to exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {NeuralNet} from "./classes/NeuralNet";
 import {sigmoid, sigmoidPrime} from "./util/activation/sigmoid";
 import {relu, reluPrime} from "./util/activation/relu";
+import {squaredError} from "./util/error/squaredError";
 
-export {NeuralNet, sigmoid, sigmoidPrime, relu, reluPrime};
+export {NeuralNet, sigmoid, sigmoidPrime, relu, reluPrime, squaredError};
